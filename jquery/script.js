e72911dc7254245b8e10770cd83421865e4fae25
CarlosCastaneda04 <54734121+CarlosCastaneda04@users.noreply.github.com>
2023-11-10 09:35:25 -0600

    ya mis onichans

--- jquery/script.js
+++ jquery/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   // Cuando se hace clic en el botón "Mostrar Datos"
   $(".mostrar-datos").click(function () {
-    const datosGuitarra = $(this).siblings(".datos-guitarra");
+    const datosInstrumento = $(this).siblings(".datos-instrumento");
     // Muestra u oculta los datos de la guitarra
-    datosGuitarra.toggle();
+    datosInstrumento.toggle();
   });
 
 
